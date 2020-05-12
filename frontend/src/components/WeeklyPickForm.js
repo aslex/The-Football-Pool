1ed1908fd89357ea1fa5933384eb5b088aebd2a1
Alexandra Servie <alexservie@MacBook-Pro-3.fritz.box>
2020-05-12 14:13:02 +0200

    picks buttons added

--- frontend/src/components/WeeklyPickForm.js
+++ frontend/src/components/WeeklyPickForm.js
@@ -5,17 +5,39 @@ export default function WeeklyPickForm(props) {
   const { getTokenSilently, user } = useAuth0();
   const [totalPoints, setTotalPoints] = useState(null);
   const [week, setWeek] = useState(null);
+  const [picks, setPicks] = useState({})
 
   useEffect(() => {
     console.log("props change!", props.games);
     setTotalPoints(props.games.length);
     setWeek(props.games[0].week);
   }, [props]);
+
   const handleClick = e => {
-    console.log(e.target.name)
+    const team = e.target.name
+    const game = e.target.id.split(' ')[0]
+    console.log('game id', game)
+    // document.getElementById(e.target.id).classList.toggle('active')
+    console.log('game object exists?', picks[game])
+    if(picks[game]){
+      console.log('IF')
+      const updatedPick = {[game]:{selection: team}}
+      setPicks(prevPicks => {return {...prevPicks, ...updatedPick } } )
+      // setPicks([...picks, picks[game].selection = team])
+    }else{
+      console.log('ELSE')
+      const pick = {
+       [game]: {
+          selection: team
+        }
+      }
+      setPicks( picks => {return {...picks, ...pick } })
+      // setPicks([...picks, pick])
+    }
   }
-  console.log("user !", user);
-  console.log("total points:", totalPoints);
+  // console.log("user !", user);
+  // console.log("total points:", totalPoints);
+  console.log('picks added to state ?!', picks)
   const submitPicks = async (e) => {
     e.preventDefault();
     console.log("submit!");
@@ -29,13 +51,16 @@ export default function WeeklyPickForm(props) {
     });
     console.log("response", response);
   };
+
+
   const renderEachGame = props.games.map((game) => {
     return (
       <div className="game" key={game.gameId}>
-        <button type="button" name={game.homeTeam} onClick={handleClick}>
+        <button type="button" className={
+          picks[game.gameId] && picks[game.gameId].selection == game.homeTeam ? 'game-button active' : 'game-button'} id={`${game.gameId} ${game.homeTeam}`} name={game.homeTeam} onClick={handleClick}>
           {game.homeTeam}
         </button> vs. 
-        <button type="button" name={game.awayTeam} onClick={handleClick}>
+        <button type="button" className={picks[game.gameId] && picks[game.gameId].selection == game.awayTeam ? 'game-button active' : 'game-button'} id={`${game.gameId} ${game.awayTeam}`} name={game.awayTeam} onClick={handleClick}>
           {game.awayTeam}
         </button>
         <label htmlFor={game.gameId}>
@@ -44,6 +69,8 @@ export default function WeeklyPickForm(props) {
       </div>
     );
   });
+
+
   return (
     <div className="pick-form">
       {totalPoints && <p>total points this week: {totalPoints}</p>}
